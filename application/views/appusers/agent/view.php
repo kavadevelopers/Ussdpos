11616591ded06fb511e7cf64814681b8ee1bfe16
Kava Developers <mehul9921@gmail.com>
2021-09-03 13:53:31 +0530

    Update view.php

--- application/views/appusers/agent/view.php
+++ application/views/appusers/agent/view.php
@@ -84,7 +84,7 @@
                                                                     <th scope="row">Mobile no.</th>
                                                                     <td>
                                                                         <?= $item->phone ?> 
-                                                                        <a href="whatsapp://send?phone=+234<?= $item->phone ?>">
+                                                                        <a href="https://web.whatsapp.com/send?phone=+234<?= $item->phone ?>">
                                                                             <img src="<?= base_url('asset/images/watsapp.png') ?>" style="width:20px;">
                                                                         </a>
                                                                     </td>
