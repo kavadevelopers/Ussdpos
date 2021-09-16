e347a91cea960d683177065a101c40260f0a2352
Kava Developers <mehul9921@gmail.com>
2021-09-16 17:39:25 +0530

    asd

--- application/controllers/api/Home.php
+++ application/controllers/api/Home.php
@@ -8,19 +8,17 @@ class Home extends CI_Controller
 
 	public function transactions()
 	{
-		$trans = $this->db->limit(10)->get_where('transactions');	
-		$transList = $trans->result_object();
-		foreach ($transList as $key => $value) {
-			$transList[$key]->typestr = ucfirst(traType($value->type)[0]);
+		if ($this->input->post('user')) {
+			$homeTransactions 	= $this->dashboard_model->getHomeTransactions($this->input->post('user'));
+			$data = [
+				'_return'		=> true,
+				'tracounter'	=> $homeTransactions['count'],
+				'tralist'		=> $homeTransactions['list']
+			];
+			retJson($data);
+		}else{
+			retJson(['_return' => false,'msg' => '`user` are Required']);
 		}
-
-		$data = [
-			'_return'		=> true,
-			'tracounter'	=> 0,
-			'tralist'		=> $transList
-		];
-
-		retJson($data);
 	}
 
 	public function get()
--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -7,6 +7,25 @@ class Dashboard_model extends CI_Model
 		parent::__construct();
 	}
 
+	public function getAllTransactions($user)
+	{
+		$this->db->order_by('id','desc');
+		$this->db->where('usertype','agent');
+		$this->db->where('user',$user);
+		$q = $this->db->get('transactions');
+		$list = $q->result_object();
+		foreach ($list as $key => $value) {
+			if ($value->type == '1') {
+				$list[$key]->title 		= "USSD Payment";
+				$list[$key]->desc 		= "Payment Received";
+				$list[$key]->at 		= getPretyDate($value->dt);
+				$list[$key]->debit 		= ptPretyAmount($value->debit);
+				$list[$key]->credit 	= ptPretyAmount($value->credit);
+			}
+		}
+		return ['count' => $q->num_rows(),'list' => $list];
+	}
+
 	public function getHomeTransactions($user)
 	{
 		$this->db->limit(10);
